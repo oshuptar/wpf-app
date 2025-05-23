66b6da4d3399a901411008917e097ccabb798193
shuptaro <01193038@pw.edu.pl>
2025-05-23 15:53:19 +0200

    Stage01,02 - done; Stage03,04-Partial

---
+++ WPF2/WPF2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using System.Security.Policy;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User User = new User();
+        ObservableCollection<Message> Messages = new ObservableCollection<Message>();
+        SendCommand sendCommand = new SendCommand();
+        NewLineCommand newLineCommand = new NewLineCommand();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Messages.Add(new Message("New message", User));
+        }
+
+        private void User_Connect(object sender, RoutedEventArgs e)
+        {
+            User.Status = true;
+            ConnectMenuItem.IsEnabled = false;
+            DisconnectMenuItem.IsEnabled = true;
+        }
+
+        private void Show_MessageBox(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a Group Chat Client!", "Group Chat", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void User_Disconnect(object sender, RoutedEventArgs e)
+        {
+            User.Status = false;
+            DisconnectMenuItem.IsEnabled = false;
+            ConnectMenuItem.IsEnabled = true;
+        }
+
+        private void App_Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ WPF2/WPF2/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF2;
+
+public class Message
+{
+    public string MessageInfo {  get; set; }
+    public User Sender { get; set; }
+
+    public Message(string messageInfo, User sender)
+    {
+        MessageInfo = messageInfo;
+        Sender = sender;
+    }
+}
